c1ee93ce242be4c17cf3fc55d0aaa6a42a07f3cb
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-10-28 14:23:10 +0300

    Refactor TodoList component

--- components/todos/TodosList.js
+++ components/todos/TodosList.js
@@ -3,8 +3,8 @@ import { Table, Checkbox, Button } from 'antd';
 const TodoList = () => {
   const columns = [
     {
-      title: 'Title', dataIndex: 'title', key: 'title',
-      sorter: (a, b) => a.title>b.title ? 1 : a.title<b.title ? -1 : 0
+      title: 'Text', dataIndex: 'text', key: 'text',
+      sorter: (a, b) => a.text>b.text ? 1 : a.text<b.text ? -1 : 0
     },
     {
       title: 'Completed', dataIndex: 'completed', key: 'completed',
@@ -37,21 +37,21 @@ const TodoList = () => {
   ];
 
   const dataSource = [
-    { id: 1, title: 'Todo 1', completed: true, key: 'todo1' },
-    { id: 2, title: 'Todo 2', completed: false, key: 'todo2' },
-    { id: 3, title: 'Todo 3', completed: true, key: 'todo3' },
-    { id: 4, title: 'Todo 4', completed: true, key: 'todo4' },
-    { id: 5, title: 'Todo 5', completed: false, key: 'todo5' },
-    { id: 6, title: 'Todo 6', completed: false, key: 'todo6' },
-    { id: 7, title: 'Todo 7', completed: true, key: 'todo7' },
-    { id: 8, title: 'Todo 8', completed: false, key: 'todo8' },
-    { id: 9, title: 'Todo 9', completed: true, key: 'todo9' },
-    { id: 10, title: 'Todo 10', completed: true, key: 'todo10' },
-    { id: 11, title: 'Todo 11', completed: false, key: 'todo11' },
-    { id: 12, title: 'Todo 12', completed: false, key: 'todo12' },
-    { id: 13, title: 'Todo 13', completed: true, key: 'todo13' },
-    { id: 14, title: 'Todo 14', completed: false, key: 'todo14' },
-    { id: 15, title: 'Todo 15', completed: true, key: 'todo15' },
+    { id: 1, text: 'Todo 1', completed: true, key: 'todo1' },
+    { id: 2, text: 'Todo 2', completed: false, key: 'todo2' },
+    { id: 3, text: 'Todo 3', completed: true, key: 'todo3' },
+    { id: 4, text: 'Todo 4', completed: true, key: 'todo4' },
+    { id: 5, text: 'Todo 5', completed: false, key: 'todo5' },
+    { id: 6, text: 'Todo 6', completed: false, key: 'todo6' },
+    { id: 7, text: 'Todo 7', completed: true, key: 'todo7' },
+    { id: 8, text: 'Todo 8', completed: false, key: 'todo8' },
+    { id: 9, text: 'Todo 9', completed: true, key: 'todo9' },
+    { id: 10, text: 'Todo 10', completed: true, key: 'todo10' },
+    { id: 11, text: 'Todo 11', completed: false, key: 'todo11' },
+    { id: 12, text: 'Todo 12', completed: false, key: 'todo12' },
+    { id: 13, text: 'Todo 13', completed: true, key: 'todo13' },
+    { id: 14, text: 'Todo 14', completed: false, key: 'todo14' },
+    { id: 15, text: 'Todo 15', completed: true, key: 'todo15' },
   ];
 
   const onChange = (pagination, filters, sorter) => {
